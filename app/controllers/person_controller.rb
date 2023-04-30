89ab9cdfa6c135388de2a9796fd4677d8aebf86e
fantasticlife <michael@smethur.st:wq>
2023-04-30 21:39:20 +0100

    added ordering to list view

--- app/controllers/person_controller.rb
+++ app/controllers/person_controller.rb
@@ -2,6 +2,6 @@ class PersonController < ApplicationController
   
   def index
     @page_title = 'People'
-    @people = Person.all
+    @people = Person.all.order( 'surname' ).order( 'forenames' )
   end
 end
