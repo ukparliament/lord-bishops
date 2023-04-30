fceb3c03f4c4805390330664328525977996a8d9
fantasticlife <michael@smethur.st:wq>
2023-04-30 21:36:13 +0100

    new established church model

---
+++ app/models/established_church.rb
@@ -0,0 +1,2 @@
+class EstablishedChurch < ApplicationRecord
+end
---
+++ test/models/established_church_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class EstablishedChurchTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
