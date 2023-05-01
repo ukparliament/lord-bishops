656d140d9589b388a439ba815e46e6585563c41d
fantasticlife <michael@smethur.st:wq>
2023-05-01 14:31:44 +0100

    added methods for dioceses: arch, named, other

--- app/models/established_church.rb
+++ app/models/established_church.rb
@@ -1,2 +1,26 @@
 class EstablishedChurch < ApplicationRecord
+  
+  def archdioceses
+    LordBishopDiocese
+      .all
+      .where( 'is_archdiocese = true' )
+      .where( 'is_named_see = true' )
+      .where( "established_church_id = ?", self.id )
+  end
+  
+  def named_dioceses
+    LordBishopDiocese
+      .all
+      .where( 'is_archdiocese = false' )
+      .where( 'is_named_see = true' )
+      .where( "established_church_id = ?", self.id )
+  end
+  
+  def other_dioceses
+    LordBishopDiocese
+      .all
+      .where( 'is_archdiocese = false' )
+      .where( 'is_named_see = false' )
+      .where( "established_church_id = ?", self.id )
+  end
 end
