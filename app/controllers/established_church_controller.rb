628b2dff3b4113b4d67c45622f2a7b8c0d649b6e
fantasticlife <michael@smethur.st:wq>
2023-05-01 11:39:21 +0100

    new method for show

--- app/controllers/established_church_controller.rb
+++ app/controllers/established_church_controller.rb
@@ -4,4 +4,10 @@ class EstablishedChurchController < ApplicationController
     @page_title = 'Established churches'
     @established_churches = EstablishedChurch.all.order( 'name' )
   end
+  
+  def show
+    established_church = params[:established_church]
+    @established_church = EstablishedChurch.find( established_church )
+    @page_title = @established_church.name
+  end
 end
