fd68ec4a970a1d916114d90d0859f20aababda28
fantasticlife <michael@smethur.st:wq>
2023-05-01 11:40:16 +0100

    new methods for dob and dod

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,7 +2,10 @@ class Person < ApplicationRecord
   
   def list_display_label
     list_display_label = self.list_display_name + ' ('  + self.dates_as_year_display + ')'
-    list_display_label
+  end
+  
+  def display_name
+    self.forenames + ' ' + self.surname
   end
   
   def list_display_name
@@ -10,16 +13,65 @@ class Person < ApplicationRecord
   end
   
   def dates_as_year_display
-    date_of_birth_display + ' - ' + date_of_death_display
+    year_of_birth_display + ' - ' + year_of_death_display
   end
   
-  def date_of_birth_display
-    date_of_birth_display = self.year_of_birth.to_s
+  def year_of_birth_display
+    self.year_of_birth.to_s
   end
   
-  def date_of_death_display
-    date_of_death_display = ''
-    date_of_death_display = self.year_of_death.to_s if self.year_of_death
-    date_of_death_display
+  def year_of_death_display
+    year_of_death_display = ''
+    year_of_death_display = self.year_of_death.to_s if self.year_of_death
+    year_of_death_display
+  end
+  
+  def lifespan
+    self.date_of_birth + ' - ' + self.date_of_death
+  end
+  
+  def date_of_birth
+    date_of_birth = ''
+    date_of_birth += self.day_of_birth.to_s + ' ' if self.day_of_birth
+    date_of_birth += month_display( self.month_of_birth ) + ' ' if self.month_of_birth
+    date_of_birth += self.year_of_birth.to_s + ' ' if self.year_of_birth
+    date_of_birth
+  end
+  
+  def date_of_death
+    date_of_death = ''
+    date_of_death += self.day_of_death.to_s + ' ' if self.day_of_death
+    date_of_death += month_display( self.month_of_death ) + ' ' if self.month_of_death
+    date_of_death += self.year_of_death.to_s + ' ' if self.year_of_death
+    date_of_death
+  end
+  
+  def month_display( month )
+    case month
+      when 1
+        'January'
+      when 2
+        'February'
+      when 3
+        'March'
+      when 4
+        'April'
+      when 5
+        'May'
+      when 6
+        'June'
+      when 7
+        'July'
+      when 8
+        'August'
+      when 9
+        'September'
+      when 10
+        'October'
+      when 11
+        'November'
+      when 12
+        'December'
+    end
   end
 end
