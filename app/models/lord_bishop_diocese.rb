1a2d7735f571c7f172b6bd71057245991109d7e1
fantasticlife <michael@smethur.st:wq>
2023-05-02 12:50:18 +0100

    moved month display method to application record

--- app/models/lord_bishop_diocese.rb
+++ app/models/lord_bishop_diocese.rb
@@ -52,35 +52,6 @@ class LordBishopDiocese < ApplicationRecord
     end_date
   end
   
-  def month_display( month )
-    case month
-      when 1
-        'January'
-      when 2
-        'February'
-      when 3
-        'March'
-      when 4
-        'April'
-      when 5
-        'May'
-      when 6
-        'June'
-      when 7
-        'July'
-      when 8
-        'August'
-      when 9
-        'September'
-      when 10
-        'October'
-      when 11
-        'November'
-      when 12
-        'December'
-    end
-  end
-  
   def lord_bishop_diocese_incumbencies
     LordBishopDioceseIncumbency.find_by_sql(
       "
