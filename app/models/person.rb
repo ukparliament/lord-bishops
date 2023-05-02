2a327f4424bdca6768c993d48fc1972b643fad5d
fantasticlife <michael@smethur.st:wq>
2023-05-02 12:51:05 +0100

    moved month display method to application record

--- app/models/person.rb
+++ app/models/person.rb
@@ -46,35 +46,6 @@ class Person < ApplicationRecord
     date_of_death
   end
   
-  def month_display( month )
-    case month
-      when 1
-        'January'
-      when 2
-        'February'
-      when 3
-        'March'
-      when 4
-        'April'
-      when 5
-        'May'
-      when 6
-        'June'
-      when 7
-        'July'
-      when 8
-        'August'
-      when 9
-        'September'
-      when 10
-        'October'
-      when 11
-        'November'
-      when 12
-        'December'
-    end
-  end
-  
   def lord_bishop_diocese_incumbencies
     LordBishopDioceseIncumbency.find_by_sql(
       "
