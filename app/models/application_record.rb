6555389cd0969a7e170b6da0ef1b7ceb60974e15
fantasticlife <michael@smethur.st:wq>
2023-05-02 12:49:57 +0100

    moved month display method to application record

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,32 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+  
+  def month_display( month )
+    case month
+      when 1
+        'January'
+      when 2
+        'February'
+      when 3
+        'March'
+      when 4
+        'April'
+      when 5
+        'May'
+      when 6
+        'June'
+      when 7
+        'July'
+      when 8
+        'August'
+      when 9
+        'September'
+      when 10
+        'October'
+      when 11
+        'November'
+      when 12
+        'December'
+    end
+  end
 end
