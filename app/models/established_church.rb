83009a4c946962961a02d1c9aae2b6ca6aa56a4a
fantasticlife <michael@smethur.st:wq>
2023-05-03 08:17:55 +0100

    split into named and unnamed dioceses, named split into named archdioceses and named non archdiocese

--- app/models/established_church.rb
+++ app/models/established_church.rb
@@ -1,26 +1,36 @@
 class EstablishedChurch < ApplicationRecord
   
-  def archdioceses
-    LordBishopDiocese
-      .all
-      .where( 'is_archdiocese = true' )
-      .where( 'is_named_see = true' )
-      .where( "established_church_id = ?", self.id )
-  end
-  
   def named_dioceses
     LordBishopDiocese
       .all
-      .where( 'is_archdiocese = false' )
       .where( 'is_named_see = true' )
       .where( "established_church_id = ?", self.id )
+      .order( 'most_recent_name')
   end
   
-  def other_dioceses
+  def named_archdioceses
     LordBishopDiocese
       .all
+      .where( 'is_named_see = true' )
+      .where( 'is_archdiocese = true' )
+      .where( "established_church_id = ?", self.id )
+      .order( 'most_recent_name')
+  end
+  
+  def named_non_archdioceses
+    LordBishopDiocese
+      .all
+      .where( 'is_named_see = true' )
       .where( 'is_archdiocese = false' )
+      .where( "established_church_id = ?", self.id )
+      .order( 'most_recent_name')
+  end
+  
+  def unnamed_dioceses
+    LordBishopDiocese
+      .all
       .where( 'is_named_see = false' )
       .where( "established_church_id = ?", self.id )
+      .order( 'most_recent_name')
   end
 end
